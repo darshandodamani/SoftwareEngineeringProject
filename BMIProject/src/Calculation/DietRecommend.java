e2c29d15cbbfe0b8b9ee5f7fbd5ba8cd5f2d1e8e
darshandodamani <darshanmd.dn@gmail.com>
2022-05-03 16:14:04 +0530

    Added DietRecommended

---
+++ BMIProject/src/Calculation/DietRecommend.java
@@ -0,0 +1,16 @@
+package Calculation;
+
+import java.util.Scanner;
+//import java.util.*;				          // this will put everything
+
+public class DietRecommend {
+
+	public static void main(String[] args) throws Exception {
+		Scanner userinput = new Scanner(System.in);
+		System.out.println("Please select the Diet: 1 for imperial, 2 for metric: ");
+		userinput.nextInt();
+		
+		//if(totalBMI == )
+	}
+
+}
